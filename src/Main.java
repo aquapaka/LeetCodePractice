3b11944bac803bcebc26477b0817f79eab8f879a
aquapaka <aqua.tamlong@gmail.com>
2022-09-05 22:31:37 +0700

    Solve CellsInRangeOnAnExcelSheet problem

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import algorithms.medium.MaxIncreaseToKeepCitySkyline;
+import algorithms.easy.CellsInRangeOnAnExcelSheet;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Programmer!");
-        int[][] array = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
 
-        System.out.println(MaxIncreaseToKeepCitySkyline.maxIncreaseKeepingSkyline(array));
+        System.out.println(CellsInRangeOnAnExcelSheet.cellsInRange("K1:L2"));
     }
 }
+
