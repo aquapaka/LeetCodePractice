82f96afed4a5a32e9d5907f8a23ad63684f80b0c
aquapaka <aqua.tamlong@gmail.com>
2022-09-05 23:28:31 +0700

    Solve SumOfAllOddLengthSubarrays problem

---
+++ src/algorithms/easy/SumOfAllOddLengthSubarrays.java
@@ -0,0 +1,54 @@
+package algorithms.easy;
+
+/*
+Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+A subarray is a contiguous subsequence of the array.
+
+Example 1:
+
+Input: arr = [1,4,2,5,3]
+Output: 58
+Explanation: The odd-length subarrays of arr and their sums are:
+[1] = 1
+[4] = 4
+[2] = 2
+[5] = 5
+[3] = 3
+[1,4,2] = 7
+[4,2,5] = 11
+[2,5,3] = 10
+[1,4,2,5,3] = 15
+If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+Example 2:
+
+Input: arr = [1,2]
+Output: 3
+Explanation: There are only 2 subarrays of odd length, [1] and [2]. Their sum is 3.
+Example 3:
+
+Input: arr = [10,11,12]
+Output: 66
+ */
+
+public class SumOfAllOddLengthSubarrays {
+    public static int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+
+        // Travel through each odd length
+        for (int currentLength = 1; currentLength <= arr.length; currentLength += 2) {
+
+            // Travel through each subArray with length = currentLength
+            for (int startIndex = 0; startIndex + currentLength <= arr.length; ++startIndex) {
+
+                // Travel through each element in subArray
+                for (int index = startIndex; index < startIndex + currentLength; ++index) {
+                    sum += arr[index];
+                }
+
+            }
+        }
+
+        return sum;
+    }
+}
