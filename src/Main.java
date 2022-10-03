40530b9d9d76128828fe8f3db88e6a7534f361b0
aquapaka <aqua.tamlong@gmail.com>
2022-10-03 13:59:22 +0700

    Solve MinimumTimeToMakeRopeColorful problem

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import algorithms.medium.MinimumTimeToMakeRopeColorful;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting Program!");
-        
+
         System.out.println(MinimumTimeToMakeRopeColorful.minCost("aabaa", new int[]{1,2,3,4,1}));
     }
 }
