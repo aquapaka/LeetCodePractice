38aba53cf8a014d7295873661cb7920b22c02bb8
aquapaka <aqua.tamlong@gmail.com>
2022-10-07 23:01:17 +0700

    Solve MyCalendarIII problem

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
-import algorithms.medium.MinimumTimeToMakeRopeColorful;
+import algorithms.hard.MyCalendarIII;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting Program!");
+        long startTime = System.currentTimeMillis();
 
-        System.out.println(MinimumTimeToMakeRopeColorful.minCost("aabaa", new int[]{1,2,3,4,1}));
+        MyCalendarIII myCalendarIII = new MyCalendarIII();
+        System.out.println(myCalendarIII.book(10, 20));
+        System.out.println(myCalendarIII.book(50, 60));
+        System.out.println(myCalendarIII.book(10, 40));
+        System.out.println(myCalendarIII.book(5, 15));
+        System.out.println(myCalendarIII.book(5, 10));
+        System.out.println(myCalendarIII.book(25, 55));
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Execution time: " + (endTime - startTime) + " ms");
+
     }
 }
 
