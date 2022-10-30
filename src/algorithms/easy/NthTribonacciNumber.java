3d3074fc49d813b02cc3018ae08f0a1ef89c7ab0
aquapaka <aqua.tamlong@gmail.com>
2022-10-31 02:00:37 +0700

    Solve NthTribonacciNumber problem

---
+++ src/algorithms/easy/NthTribonacciNumber.java
@@ -0,0 +1,41 @@
+package algorithms.easy;
+
+/*
+The Tribonacci sequence Tn is defined as follows:
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+
+Example 1:
+
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+Example 2:
+
+Input: n = 25
+Output: 1389537
+ */
+
+public class NthTribonacciNumber {
+    public int tribonacci(int n) {
+        int[] sequenceArr = new int[n + 1];
+
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+
+        sequenceArr[0] = 0;
+        sequenceArr[1] = 1;
+        sequenceArr[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            sequenceArr[i] = sequenceArr[i - 1] + sequenceArr[i - 2] + sequenceArr[i - 3];
+        }
+
+        return sequenceArr[n];
+    }
+}
