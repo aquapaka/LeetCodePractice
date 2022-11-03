4d34eb21ac20678965a9ba2e8d4c69ff61498ebe
aquapaka <aqua.tamlong@gmail.com>
2022-11-03 19:16:26 +0700

    Update main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
-import algorithms.easy.RemoveDuplicatesFromSortedArray;
-
-import java.util.Arrays;
+import algorithms.hard.MyCalendarIII;
+import algorithms.medium.ThreeSumClosest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting test...");
-
+        System.out.println("Starting Program!");
         long startTime = System.currentTimeMillis();
 
-        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
-        System.out.println(RemoveDuplicatesFromSortedArray.removeDuplicates(nums));
-        System.out.println(Arrays.toString(nums));
+        /*
+            - Execution here ----------------------------------
+         */
+        MyCalendarIII myCalendarIII = new MyCalendarIII();
+        System.out.println(ThreeSumClosest.threeSumClosest(new int[]{-1,2,1,-4}, 1));
+        /*
+            ----------------------------------------------------
+         */
 
         long endTime = System.currentTimeMillis();
-        System.out.println("Finished! Execution time: " + (endTime - startTime) + " ms");
+        System.out.println("Execution time: " + (endTime - startTime) + " ms");
+
     }
 }
 
